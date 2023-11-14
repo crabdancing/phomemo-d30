99cc28649750db22f29b452f1c6f868fc2c38e2e
Alexandria P <alxpettit@gmail.com>
2023-11-13 17:31:40 -0700

    fix library paths

--- flake.nix
+++ flake.nix
@@ -12,27 +12,40 @@
           inherit system;
         };
 
-        baseInputs = with pkgs; [ pkg-config systemd.dev bluez.dev fontconfig.dev ];
+        lib = pkgs.lib;
+        guiInputs = with pkgs; with pkgs.xorg; [ libX11 libXcursor libXrandr libXi vulkan-loader libxkbcommon wayland ];
+        buildInputs = with pkgs; [ pkg-config systemd bluez fontconfig ];
+        LD_LIBRARY_PATH = lib.makeLibraryPath (buildInputs ++ guiInputs);
+
+        commonEnvironment = {
+          inherit buildInputs;
+        };
 
         naersk' = pkgs.callPackage naersk {};
 
-        mkCliBuild = pname: naersk'.buildPackage {
+        mkCliBuild = pname: naersk'.buildPackage (lib.recursiveUpdate commonEnvironment {
           inherit pname;
           src = ./.; # Adjust the source path according to your workspace layout
-          nativeBuildInputs = with pkgs; [ pkg-config cmake ];
-          buildInputs = baseInputs;
+          nativeBuildInputs = with pkgs; [ pkg-config cmake makeWrapper ];
+          buildInputs = buildInputs;
           cargoBuildOptions = opts: opts ++ [ "--package" pname ];
-        };
+          postInstall = ''
+            wrapProgram "$out/bin/d30-cli" \
+              --prefix LD_LIBRARY_PATH : "${LD_LIBRARY_PATH}"
+          '';
+        });
 
-      in rec {
+      in {
         defaultPackage = mkCliBuild "d30-cli";
         d30-cli = mkCliBuild "d30-cli";
 
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ] ++ baseInputs;
-          PKG_CONFIG_PATH = "${pkgs.systemd.dev}/lib/pkgconfig:${pkgs.bluez.dev}/lib/pkgconfig${pkgs.fontconfig.dev}/lib/pkgconfig:${pkgs.freetype.dev}/lib/pkgconfig";
-        };
+        devShell = pkgs.mkShell (lib.recursiveUpdate commonEnvironment {
+          inherit LD_LIBRARY_PATH;
+          shellHook = ''
+            exec $SHELL
+          '';
+          nativeBuildInputs = with pkgs; [ rustc cargo ] ++ buildInputs;
+        });
       }
     );
 }
