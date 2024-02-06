fc3014f085f92ef61cfd51e362db5fec77ac7469
Alexandria P. <alxpettit@gmail.com>
2024-02-06 15:29:44 -0700

    more cleanup

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -135,11 +135,6 @@ enum Accepted {
     Unknown,
 }
 
-// fn backend_miniview(preview_image: DynamicImage) -> Result<Accepted, Whatever> {
-//     let window = miniview::ConfigBuilder::set_lazy_window(, )
-//     // let window = WindowMouseButtonEvent
-// }
-
 fn backend_show_image(preview_image: DynamicImage) -> Result<Accepted, Whatever> {
     let mut accepted = Accepted::Unknown;
 
@@ -186,68 +181,6 @@ fn backend_show_image(preview_image: DynamicImage) -> Result<Accepted, Whatever>
             }
         }
     }
-
-    // let window = show_image::create_window("image", Default::default())
-    //     .with_whatever_context(|_| "Could not create window for preview")?;
-    // window
-    //     .set_image("image-001", preview_image)
-    //     .with_whatever_context(|_| "Could not set image")?;
-    // 'event_loop: for event in window
-    //     .event_channel()
-    //     .with_whatever_context(|_| "Could not handle window channel")?
-    // {
-    //     match event {
-    //         // show_image::event::WindowEvent::RedrawRequested(_) => todo!(),
-    //         // show_image::event::WindowEvent::Resized(_) => todo!(),
-    //         // show_image::event::WindowEvent::Moved(_) => todo!(),
-    //         show_image::event::WindowEvent::CloseRequested(_) => {
-    //             accepted = Accepted::Unknown;
-    //             break 'event_loop;
-    //         }
-    //         show_image::event::WindowEvent::Destroyed(_) => {
-    //             accepted = Accepted::Unknown;
-    //             break 'event_loop;
-    //         }
-    //         // show_image::event::WindowEvent::DroppedFile(_) => todo!(),
-    //         // show_image::event::WindowEvent::HoveredFile(_) => todo!(),
-    //         // show_image::event::WindowEvent::HoveredFileCancelled(_) => todo!(),
-    //         // show_image::event::WindowEvent::FocusGained(_) => todo!(),
-    //         show_image::event::WindowEvent::FocusLost(_) => {
-    //             accepted = Accepted::Unknown;
-    //             break 'event_loop;
-    //         }
-    //         show_image::event::WindowEvent::KeyboardInput(input) => match input {
-    //             WindowKeyboardInputEvent { input, .. } => match input {
-    //                 show_image::event::KeyboardInput { key_code, .. } => {
-    //                     match key_code {
-    //                         Some(show_image::event::VirtualKeyCode::Y) => {
-    //                             accepted = Accepted::Yes;
-    //                             break 'event_loop;
-    //                         }
-    //                         Some(show_image::event::VirtualKeyCode::N) => {
-    //                             accepted = Accepted::No;
-    //                             break 'event_loop;
-    //                         }
-    //                         _ => {}
-    //                     }
-    //                     dbg!(input);
-    //                 }
-    //             },
-    //         },
-    //         // show_image::event::WindowEvent::TextInput(_) => todo!(),
-    //         // show_image::event::WindowEvent::MouseEnter(_) => todo!(),
-    //         // show_image::event::WindowEvent::MouseLeave(_) => todo!(),
-    //         // show_image::event::WindowEvent::MouseMove(_) => todo!(),
-    //         // show_image::event::WindowEvent::MouseButton(_) => todo!(),
-    //         // show_image::event::WindowEvent::MouseWheel(_) => todo!(),
-    //         // show_image::event::WindowEvent::AxisMotion(_) => todo!(),
-    //         // show_image::event::WindowEvent::TouchpadPressure(_) => todo!(),
-    //         // show_image::event::WindowEvent::Touch(_) => todo!(),
-    //         // show_image::event::WindowEvent::ScaleFactorChanged(_) => todo!(),
-    //         // show_image::event::WindowEvent::ThemeChanged(_) => todo!(),
-    //         _ => {}
-    //     }
-    // }
     Ok(Accepted::Unknown)
 }
 fn cmd_show_preview(
@@ -422,7 +355,6 @@ fn cmd_print(config: &mut Config, args: &ArgsPrintText) -> Result<(), Whatever> 
 
 #[snafu::report]
 #[tokio::main]
-#[show_image::main]
 async fn main() -> Result<(), Whatever> {
     env_logger::init_from_env(
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "warn,naga=off"),
